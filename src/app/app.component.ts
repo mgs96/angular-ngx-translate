f3aed75c57c0882891af5192c084ea73a8328945
Mauricio <mauricio.guzman.salazar@gmail.com>
2018-08-01 17:02:03 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  private isEnglish = true;
+  private isDeutsch = false;
+  private isSpanish = false;
+
+  constructor(
+    private translate: TranslateService
+  ) {
+    translate.setDefaultLang('en');
+  }
+
+  useLanguage(lang: string) {
+    this.changeActiveButton(lang);
+    this.translate.use(lang);
+  }
+  changeActiveButton(lang: string) {
+    switch (lang) {
+      case 'en':
+        this.isEnglish = true;
+        this.isDeutsch = false;
+        this.isSpanish = false;
+        break;
+      case 'de':
+        this.isEnglish = false;
+        this.isDeutsch = true;
+        this.isSpanish = false;
+        break;
+      case 'es':
+        this.isEnglish = false;
+        this.isDeutsch = false;
+        this.isSpanish = true;
+        break;
+      default:
+        this.isEnglish = true;
+        this.isDeutsch = false;
+        this.isSpanish = false;
+        break;
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +11,22 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
